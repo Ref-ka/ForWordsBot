3a7f0b7ab55434f4b278de9eeb048528dcf6e0c6
Ref_ka <228refka1337@gmail.com>
2024-11-02 15:52:23 +0500

    disable whisper for faster testing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
-import whisper
+# import whisper
 from database import DataBase
 from os import remove
 import config
@@ -16,8 +16,8 @@ upload_cache = {}
 delete_cache = {}
 edit_cache = {}
 
-model = whisper.load_model("medium", device="cuda")
-whisper.DecodingOptions(fp16=False)
+# model = whisper.load_model("medium", device="cuda")
+# whisper.DecodingOptions(fp16=False)
 
 
 @bot.message_handler(commands=['start'])
@@ -36,33 +36,33 @@ def process_eng_word(message):
     if message.chat.id not in load_cache:
         load_cache[message.chat.id] = []
     if message.text:
-        load_cache[message.chat.id].append(message.text)
-    elif message.voice.file_id:
-        file_info = bot.get_file(message.voice.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        with open(f'{message.chat.id}.ogg', 'wb') as file:
-            file.write(downloaded_file)
-        # model = whisper.load_model("base")
-        # whisper.DecodingOptions(language='eng', fp16=False)
-        load_cache[message.chat.id].append(model.transcribe(f'{message.chat.id}.ogg', language='en')['text'])
-        remove(f'{message.chat.id}.ogg')
+        load_cache[message.chat.id].append(message.text.lower())
+    # elif message.voice.file_id:
+    #     file_info = bot.get_file(message.voice.file_id)
+    #     downloaded_file = bot.download_file(file_info.file_path)
+    #     with open(f'{message.chat.id}.ogg', 'wb') as file:
+    #         file.write(downloaded_file)
+    #     # model = whisper.load_model("base")
+    #     # whisper.DecodingOptions(language='eng', fp16=False)
+    #     load_cache[message.chat.id].append(model.transcribe(f'{message.chat.id}.ogg', language='en')['text'])
+    #     remove(f'{message.chat.id}.ogg')
 
-    msg = bot.reply_to(message, 'Send to me word on russian')
+    msg = bot.reply_to(message, "Send to me word on russian")
     bot.register_next_step_handler(msg, process_ru_word)
 
 
 def process_ru_word(message):
     if message.text:
         load_cache[message.chat.id].append(message.text)
-    elif message.voice.file_id:
-        file_info = bot.get_file(message.voice.file_id)
-        downloaded_file = bot.download_file(file_info.file_path)
-        with open(f'{message.chat.id}.ogg', 'wb') as file:
-            file.write(downloaded_file)
-        # model = whisper.load_model("base")
-        # whisper.DecodingOptions(language='ru', fp16=False)
-        load_cache[message.chat.id].append(model.transcribe(f'{message.chat.id}.ogg', language='ru')['text'])
-        remove(f'{message.chat.id}.ogg')
+    # elif message.voice.file_id:
+    #     file_info = bot.get_file(message.voice.file_id)
+    #     downloaded_file = bot.download_file(file_info.file_path)
+    #     with open(f'{message.chat.id}.ogg', 'wb') as file:
+    #         file.write(downloaded_file)
+    #     # model = whisper.load_model("base")
+    #     # whisper.DecodingOptions(language='ru', fp16=False)
+    #     load_cache[message.chat.id].append(model.transcribe(f'{message.chat.id}.ogg', language='ru')['text'])
+    #     remove(f'{message.chat.id}.ogg')
     print(message.chat.id, load_cache[message.chat.id][0], load_cache[message.chat.id][1])
     print(type(message.chat.id), type(load_cache[message.chat.id][0]), type(load_cache[message.chat.id][1]))
     db.input_words(message.chat.id, load_cache[message.chat.id][0], load_cache[message.chat.id][1])
